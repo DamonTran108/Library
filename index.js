bc9ee7674816183a1ae9afec28c2227f1431f842
Lozdadon <34318050+Lozdadon@users.noreply.github.com>
2024-09-16 23:54:56 +0100

    Refreshed form after submitting

--- index.js
+++ index.js
@@ -4,116 +4,62 @@ let form = document.getElementById("form");
 
 let list = document.querySelector(".listOfBooks");
 
-
 console.log(list.children);
 
-
 function Book(title, author, numOfPages, beenRead) {
+  this.title = title;
 
-    this.title = title;
+  this.author = author;
 
-    this.author = author;
-
-    this.beenRead = beenRead;
-
-
-
+  this.beenRead = beenRead;
 }
-
 
 function addBookToLibrary() {
+  let bookTitle = document.querySelector(".bookName").value;
 
-    
+  let bookAuthor = document.querySelector(".bookAuthor").value;
 
+  let beenRead = true;
 
-    let bookTitle = document.querySelector(".bookName").value;
+  let book = new Book(bookTitle, bookAuthor, beenRead);
 
-    let bookAuthor = document.querySelector(".bookAuthor").value
-
-    let beenRead = true;
-
-    
-    let book = new Book(bookTitle, bookAuthor, beenRead);
-
-    myLibrary.push(book);
-    
-
-
+  myLibrary.push(book);
 }
 
+function getAllBooks() {
+  let book = document.createElement("li");
 
+  let bookTitle = document.createElement("div");
 
-function getAllBooks(){
+  bookTitle.innerHTML = JSON.stringify(myLibrary[myLibrary.length - 1]);
 
- 
+  book.appendChild(bookTitle);
 
-    
-
-
-
-        
-        let book = document.createElement("li");
-
-        let bookTitle = document.createElement("div");
-
-        bookTitle.innerHTML = JSON.stringify(myLibrary[myLibrary.length-1]);;
-
-        book.appendChild(bookTitle);
-        
-        list.appendChild(book);
-
-    
-    
-
-
-
-
-
+  list.appendChild(book);
 }
-
 
 let addBookBtn = document.querySelector(".addBookBtn");
 
-
 addBookBtn.addEventListener("click", (event) => {
-
-
-    form.style.display="flex";
-
-
-
-    
-})
-
-
+  form.style.display = "flex";
+});
 
 let submitBookBtn = document.querySelector(".submitBookBtn");
 
-
 submitBookBtn.addEventListener("click", (event) => {
+  addBookToLibrary();
+  console.log("hi");
 
+  console.log(myLibrary);
 
-    addBookToLibrary();
-    console.log("hi");
+  form.style.display = "none";
 
-    console.log(myLibrary);
+  console.log(list.children);
+  console.log(myLibrary);
+  getAllBooks();
 
-    form.style.display = "none"
+  document.querySelector(".bookName").value = "";
+  document.querySelector(".bookAuthor").value = "";
 
-    console.log(list.children);
-    console.log(myLibrary);
-    getAllBooks();
-
- 
-    let bookTitle = document.querySelector(".bookName").value;
-
-    let bookAuthor = document.querySelector(".bookAuthor").value
-
-    bookTitle.value = "";
-    bookAuthor.value = "";
-
-
-    event.preventDefault();
-    
-})
-
+  event.preventDefault();
+});
